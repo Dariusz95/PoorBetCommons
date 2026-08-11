777f9fe92597c13027615d1251ed6785769f3664
Dariusz Bosko <dariuszbosko95@gmail.com>
2026-08-11 14:49:41 +0200

    added error codes

--- src/main/java/com/poorbet/commons/commons/error/ErrorCode.java
+++ src/main/java/com/poorbet/commons/commons/error/ErrorCode.java
@@ -1,8 +1,11 @@
 package com.poorbet.commons.commons.error;
 
 public enum ErrorCode {
+    TEAM_NOT_FOUND,
+    TEAM_ALREADY_EXISTS,
     INSUFFICIENT_FUNDS,
     WALLET_NOT_FOUND,
+    WALLET_UNAVAILABLE,
     VALIDATION_ERROR,
     INTERNAL_ERROR
 }
